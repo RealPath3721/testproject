e07dd1ce31f321586bba2de69925b2a780440e36
realpath <realpath3721@gmail.com>
2023-08-14 09:40:40 +0300

    'init'

---
+++ src/views/AboutView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="about">
+    <div class="showcase"><div class="card lightning / interactive masked active" data-number="160" data-set="swsh12pt5" data-subtypes="basic" data-supertype="pokémon" data-rarity="rare secret" data-trainer-gallery="false"
+      style="
+      --pointer-x: 50%;
+      --pointer-y: 50%;
+      --pointer-from-center: 0;
+      --pointer-from-top: 0.5;
+      --pointer-from-left: 0.5;
+      --card-opacity: 0;
+      --rotate-x: 0deg;
+      --rotate-y: 0deg;
+      --background-x: 50%;
+      --background-y: 50%;
+      --card-scale: 1;
+      --translate-x: 0px;
+      --translate-y: 0px;"
+  >
+  <div class="card__translater"><button class="card__rotator" aria-label="Expand the Pokemon Card; Pikachu." tabindex="0"><img class="card__back" src="https://tcg.pokemon.com/assets/img/global/tcg-card-back-2x.jpg" alt="The back of a Pokemon Card, a Pokeball in the center with Pokemon logo above and below" loading="lazy" width="660" height="921"> <div class="card__front" style="
+    --seedx: 0.06591560750732928;
+    --seedy: 0.14243597899050942;
+    --cosmosbg: 48px 182px;
+    --mask: url(https://poke-holo.b-cdn.net/foils/swsh12pt5/masks/upscaled/160_foil_etched_swsecret_2x.webp);
+    --foil: url(https://poke-holo.b-cdn.net/foils/swsh12pt5/foils/upscaled/160_foil_etched_swsecret_2x.webp);
+      "><img src="https://images.pokemontcg.io/swsh12pt5/160_hires.png" alt="Front design of the Pikachu Pokemon Card, with the stats and info around the edge" loading="lazy" width="660" height="921"> <div class="card__shine"></div> <div class="card__glare"></div></div></button></div></div></div>
+  </div>
+</template>
